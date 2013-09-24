2fba4593c7645e128937aca6e7072efb2e1acbf9
shivakrishnab <shiva@agiliq.com>
2013-09-24 17:03:42 +0530

    Converted django 1.4 to 1.5

--- answrs/models.py
+++ answrs/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-
-import Image
 from django.conf import settings
+
 import os.path
 import re
 import shutil
--- answrs/urls.py
+++ answrs/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template
 from django.contrib.auth.views import login, logout
 
 urlpatterns = patterns('answrs.views',
